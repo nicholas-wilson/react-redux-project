afcdb8fb67ebddca1f3d147515826f1fbbdf7c45
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-08-27 18:45:19 -0700

    add initial components

---
+++ react-redux-project/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Play</li>
+          <li>Hiscores</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default NavBar
---
+++ react-redux-project/src/components/Play.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Play extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>This is the Play Component page!!</h1>
+      </div>
+    )
+  }
+}
+
+export default Play
