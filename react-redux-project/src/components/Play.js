665a48cb69b629fba9abd11aa7c85e528e1994ff
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-08-29 16:06:06 -0700

    added in the volleyball net component

--- react-redux-project/src/components/Play.js
+++ react-redux-project/src/components/Play.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
+import VolleyBallNet from './VolleyballNet';
 
 class Play extends Component {
 
   render() {
 
     return (
-      <div>
+      <div className="Play">
         <h1>This is the Play Component page!!</h1>
+        {/* I need a volleyball that will be moved around */}
+        {/* Timer component that counts up */}
+        <VolleyBallNet/>
       </div>
     )
   }
---
+++ react-redux-project/src/components/VolleyballNet.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import volleyballNet from '../images/volleyballNet.png';
+
+class VolleyBallNet extends Component {
+  render() {
+    return(
+      <img className="VolleyballNet" src={volleyballNet} alt="volleyball net"></img>
+    )
+  }
+}
+
+export default VolleyBallNet
