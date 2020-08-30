50d8bf515b83d00242967021ee2d72de28ac5500
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-08-29 18:15:02 -0700

    added redux tools

--- react-redux-project/src/index.js
+++ react-redux-project/src/index.js
@@ -7,8 +7,10 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
