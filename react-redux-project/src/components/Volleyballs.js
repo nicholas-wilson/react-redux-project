cca689e07715d01eb6138e3d45213647f3c82b79
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-09-03 14:44:04 -0700

    added an interval for the volleyballs to be launched at

--- react-redux-project/src/components/Volleyballs.js
+++ react-redux-project/src/components/Volleyballs.js
@@ -4,7 +4,11 @@ import Volleyball from './Volleyball';
 class Volleyballs extends Component {
   state = {
     volleyballs: [],
+    activeBalls: []
   }
+
+  launchInterval = null;
+  currentBall = 0;
 
   componentDidMount() {
     fetch('http://localhost:3000/volleyballs')
@@ -16,18 +20,42 @@ class Volleyballs extends Component {
     this.setState({
       volleyballs: balls
     })
+    this.startLaunching()
   }
 
-  renderVolleyballs = () => {
-    return this.state.volleyballs.map(ball =>
-      <Volleyball key={ball.id} size={30} left={ball.left} top={ball.top} increaseScore={this.props.increaseScore}/>
-    );
+  startLaunching = () => {
+    this.launchInterval = setInterval(this.activateVolleyball, 1000)
+  }
+
+  activateVolleyball = () => {
+    this.setState(prevState => {
+      return {
+        activeBalls: [
+          ...prevState.activeBalls,
+            <Volleyball
+              key={prevState.volleyballs[this.currentBall].id}
+              size={30}
+              left={prevState.volleyballs[this.currentBall].left}
+              top={prevState.volleyballs[this.currentBall].top}
+              increaseScore={this.props.increaseScore}
+            />
+          ]
+      }
+    });
+    this.currentBall++;
+    if (this.currentBall === 20) {
+      clearInterval(this.launchInterval)
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.launchInterval)
   }
 
   render() {
     return(
       <div>
-        {this.renderVolleyballs()}
+        {this.state.activeBalls || null}
       </div>
 
     )
