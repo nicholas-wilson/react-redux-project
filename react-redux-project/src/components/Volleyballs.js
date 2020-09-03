a8339d6ef6f8346f72014c38a1d48570b44fc773
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-09-02 17:24:02 -0700

    adjusted the servers to communicate together

--- react-redux-project/src/components/Volleyballs.js
+++ react-redux-project/src/components/Volleyballs.js
@@ -2,9 +2,26 @@ import React, { Component } from "react";
 import Volleyball from './Volleyball';
 
 class Volleyballs extends Component {
+  state = {
+    volleyballs: [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/volleyballs')
+    .then(response => response.json())
+    .then(ballsJson => this.updateState(ballsJson))
+  }
+
+  updateState = (balls) => {
+    this.setState({
+      volleyballs: balls
+    })
+  }
 
   renderVolleyballs = () => {
-    return (<Volleyball size={30} left={600} top={200} increaseScore={this.props.increaseScore}/>);
+    return this.state.volleyballs.map(ball =>
+      <Volleyball key={ball.id} size={30} left={ball.left} top={ball.top} increaseScore={this.props.increaseScore}/>
+    );
   }
 
   render() {
