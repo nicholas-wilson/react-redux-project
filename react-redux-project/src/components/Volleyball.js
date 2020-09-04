3c858704dbee2a834bea07d9f164ea41365bf50f
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-09-04 14:06:26 -0700

    added logic and style to make volleyballs move away and clear from screen after they are clicked

--- react-redux-project/src/components/Volleyball.js
+++ react-redux-project/src/components/Volleyball.js
@@ -5,27 +5,43 @@ class Volleyball extends Component {
   state = {
     top: this.props.top,
     left: this.props.left,
-    size: this.props.size
+    size: this.props.size,
+    movingForward: true,
+    class: '',
   }
 
   interval = null
 
   animateBall = ()=> {  
     
-    if (this.state.size > 80) {
+    if (this.state.size > 100 || this.state.size < 5) {
       clearInterval(this.interval);
+      this.setState({class: "hidden"})
     } else {
       this.setState(prevState => (this.getAnimationObject(prevState)))
     }
   }
 
   getAnimationObject = (prevState) => {
-    if (this.props.top === 200 && this.props.left <= 700) { // balls moving right and down
-      return {top: prevState.top + 10, left: prevState.left + 5, size: prevState.size + 5}
-    } else if (this.props.top === 200) {  // balls moving right and down
-      return {top: prevState.top + 10, left: prevState.left - 5, size: prevState.size + 5}
-    } else { // balls moving up
-      return {top: prevState.top -10, left: prevState.left + 5, size: prevState.size + 5}
+    if (this.state.movingForward){
+      if (this.props.top === 200 && this.props.left <= 700) { // balls moving right and down
+       return {top: prevState.top + 10, left: prevState.left + 5, size: prevState.size + 5}
+      } else if (this.props.top === 200) {  // balls moving right and down
+        return {top: prevState.top + 10, left: prevState.left - 5, size: prevState.size + 5}
+      } else { // balls moving up
+       return {top: prevState.top -10, left: prevState.left + 5, size: prevState.size + 5}
+      }
+    } else {
+      return {top: prevState.top + 15, left: prevState.left - 1, size: prevState.size - 10}
+    }
+  }
+
+  handleClick = () => {
+    if (this.state.size > 75) {
+      this.props.increaseScore()
+      this.setState({
+        movingForward: false
+      })
     }
   }
 
@@ -39,11 +55,11 @@ class Volleyball extends Component {
 
   render() {
     return(
-      <div id="volleyball" style={{position: 'fixed', top: `${this.state.top}px`, left: `${this.state.left}px`}}>
+      <div className={this.state.class} id="volleyball" style={{position: 'fixed', top: `${this.state.top}px`, left: `${this.state.left}px`}}>
         <img 
           src={volleyball} alt="Volleyball"
           width={this.state.size} height={this.state.size}
-          onClick={this.props.increaseScore}
+          onClick={this.handleClick}
         />
       </div>
     )
